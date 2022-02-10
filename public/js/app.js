3d934d490c05edcefaeea68cad676d9179cec5e0
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2022-02-10 20:56:46 +0300

    Adding and replying to comments Linked to Database
    now you can add new comments and reply on other people's comments and that will be saved to the database

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,33 @@
-const scoreValue = document.querySelector("#score-value");
-const addButton = document.querySelector("#add");
-const minusButton = document.querySelector("#minus");
-const replyButton = document.querySelector(".reply");
-const replyCard = document.querySelector(".reply-card");
+const scoreValue = document.querySelectorAll(".score-value");
+const addButton = document.querySelectorAll(".add");
+const minusButton = document.querySelectorAll(".minus");
+const replyButton = document.querySelectorAll(".reply");
+const replyCard = document.querySelectorAll(".reply-card")[0];
 
-addButton.addEventListener("click", function () {
-    scoreValue.textContent = parseInt(scoreValue.textContent) + 1
-})
-minus.addEventListener("click", function () {
-    if (parseInt(scoreValue.textContent) > 0)
-        scoreValue.textContent = parseInt(scoreValue.textContent) - 1
-})
-replyButton.addEventListener("click", function () {
-    const clonedReply = replyCard.cloneNode(true);
-    clonedReply.style.display = "flex";
-    this.parentElement.parentElement.parentElement.after(clonedReply);
-})
+// addButton.addEventListener("click", function () {
+//     scoreValue.textContent = parseInt(scoreValue.textContent) + 1
+// })
+
+// minus.addEventListener("click", function () {
+//     if (parseInt(scoreValue.textContent) > 0)
+//         scoreValue.textContent = parseInt(scoreValue.textContent) - 1
+// })
+
+console.log(replyCard)
+console.log(replyButton)
+let pressedButton = [];
+
+for (let i = 0; i < replyButton.length; i++) {
+    replyButton[i].addEventListener("click", function () {
+        if (!pressedButton.includes(i)) {
+            const clonedReply = replyCard.cloneNode(true);
+            clonedReply.style.display = "flex";
+            this.parentElement.parentElement.parentElement.after(clonedReply);
+            clonedReply.querySelector('.replyed-comment').innerHTML = "@" + this.getAttribute('name') + ", ";
+            clonedReply.querySelector('.repliedAt').setAttribute("value", this.getAttribute('name'));
+            clonedReply.querySelector('.commentId').setAttribute("value", this.getAttribute('id'));
+            console.log(this.getAttribute('name'));
+            pressedButton.push(i);
+        }
+    })
+}
--- server.js
+++ server.js
@@ -44,6 +44,24 @@ const commentSchema = new mongoose.Schema({
 const Comment = mongoose.model("Comment", commentSchema);
 const Reply = mongoose.model("Reply", replySchema);
 
+function CommentOb(id, content, createdAt, score, username, replies) {
+    this.id = id;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.score = score;
+    this.username = username;
+    this.replies = replies;
+}
+function RepliesOb(id, content, createdAt, score, username, replyingTo) {
+    this.id = id;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.score = score;
+    this.username = username;
+    this.replyingTo = replyingTo;
+}
+
+
 
 
 // const newComment = new Comment({
@@ -57,34 +75,35 @@ const Reply = mongoose.model("Reply", replySchema);
 
 
 
-const newComment = new Comment({
-    id: 2,
-    content: "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-    createdAt: "2 weeks ago",
-    score: 5,
-    username: "maxblagun",
-    replies: [
-        {
-            id: 3,
-            content: "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-            createdAt: "1 week ago",
-            score: 4,
-            replyingTo: "maxblagun",
-            username: "ramsesmiron"
-        },
-        {
-            id: 4,
-            content: "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-            createdAt: "2 days ago",
-            score: 2,
-            replyingTo: "ramsesmiron",
-            user: "juliusomo"
-        }
-    ]
-})
+// const newComment2 = new Comment({
+//     id: 2,
+//     content: "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+//     createdAt: "2 weeks ago",
+//     score: 5,
+//     username: "maxblagun",
+//     replies: [
+//         {
+//             id: 3,
+//             content: "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+//             createdAt: "1 week ago",
+//             score: 4,
+//             replyingTo: "maxblagun",
+//             username: "ramsesmiron"
+//         },
+//         {
+//             id: 4,
+//             content: "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+//             createdAt: "2 days ago",
+//             score: 2,
+//             replyingTo: "ramsesmiron",
+//             username: "juliusomo"
+//         }
+//     ]
+// })
 
 // newComment.save();
-Comment.find({}).then(data => console.log(data));
+// newComment2.save();
+// Comment.find({}).then(data => console.log(data));
 
 app.get("/", function (req, res) {
     const name = "Mohamed Hosam";
@@ -102,12 +121,34 @@ app.get("/comments", function (req, res) {
         if (err)
             console.log(err)
         else {
-            console.log(docs)
+            console.log(docs[1]._id.valueOf())
             res.render("comments", { currentId: currentuser, currentUsername: users[currentuser - 1], data: docs });
         }
     })
     // console.log();
 
+})
+app.post("/newComment", async function (req, res) {
+    let newId = users.indexOf(req.body.username) + 1;
+    let obj = new CommentOb(newId, req.body.content, "1 min ago", 0, req.body.username, []);
+    let d = new Comment(obj);
+    await d.save();
+    res.redirect('/comments');
+})
+// id, content, createdAt, score, username, replyingTo
+app.post("/reply", async function (req, res) {
+    let newId = users.indexOf(req.body.username) + 1;
+    let obj = new RepliesOb(newId, req.body.content, "1 min ago", 0, req.body.username, req.body.repliedAt);
+    console.log(obj)
+    Comment.findById(req.body.commentId, async function (err, docs) {
+        if (err)
+            console.log(err)
+        else {
+            docs.replies.push(obj)
+            await docs.save();
+            res.redirect('/comments');
+        }
+    })
 })
 
 
